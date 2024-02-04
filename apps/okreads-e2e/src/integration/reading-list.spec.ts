fcac418a24f4359f2499319fa3b7aaa9dfb392d2
Amitoj Singh <amitojsingh@Amitojs-MacBook.lan>
2024-02-03 22:24:11 -0600

    Add more tests

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -20,8 +20,8 @@ it('Then: clicking on complete button removes the book from Reading List',() =>{
       cy.get('[data-testing="complete-button"]').first().click();
       //Number of books in reading list should be decreased by one
         cy.get('[data-testing="complete-button"]').should('have.length', lengthOfAddedButtons - 1);
-      });
-
+      }
+    );
 });
 
 it('Then: clicking on complete button adds the book to Completed List',() =>{
@@ -30,12 +30,11 @@ it('Then: clicking on complete button adds the book to Completed List',() =>{
       cy.get('[data-testing="complete-button"]').first().click();
       //Number of books in completed list should be increased by one
         cy.get('[data-testing="remove-completed-button"]').should('have.length', lengthOfAddedButtons + 1);
-      });
-
+      }
+    );
 });
 
 it('Then: clicking on Remove completed book button removes book from Completed List',() =>{
-  
   cy.get('[data-testing="remove-completed-button"]').then((initalLengthOfAddedButtons)=>{
     const lengthOfAddedButtons = Cypress.$(initalLengthOfAddedButtons).length;
     cy.get('[data-testing="remove-completed-button"]').first().click();
@@ -46,3 +45,23 @@ it('Then: clicking on Remove completed book button removes book from Completed L
 
 });
 
+describe('When: I use the search and reading list feature', () => {
+  beforeEach(() => {
+    cy.startAt('/');
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get('[data-testing="want-to-read-btn"]').first().click();
+    cy.get('[data-testing="toggle-reading-list"]').click();
+  });
+
+it('Then: clicking on complete button removes the book from Reading List',() =>{
+  cy.get('[data-testing="remove-completed-button"]').then((initalLengthOfAddedButtons)=>{
+    const lengthOfAddedButtons = Cypress.$(initalLengthOfAddedButtons).length;
+      cy.get('[data-testing="complete-button"]').first().click();
+        cy.get('[data-testing="close-list-button"]').click();
+        cy.get('[data-testing-finish="finished"]').should('have.length', lengthOfAddedButtons + 1);
+      }
+    );
+});
+
+});
